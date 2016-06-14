870193fa4a20cc71c234937354b36d0eb6017691
Ivan <Ivan@Ivan-PC>
2016-06-14 15:15:02 +0300

    update1 ServletContext change

--- MVC_AndJavaBean/src/controller/ListServlet.java
+++ MVC_AndJavaBean/src/controller/ListServlet.java
@@ -1,10 +1,16 @@
 package controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.sun.xml.internal.ws.client.RequestContext;
 
 import classFiles.DBUser;
 import classFiles.User;
@@ -49,9 +55,12 @@ public class ListServlet extends HttpServlet {
         }
         
         // forward request and response
-        getServletContext()
-            .getRequestDispatcher(url)
-            .forward(request, response);
+        ServletContext sc = getServletContext();
+        sc.getRequestDispatcher(url).forward(request, response);;
+        
+
+
+      
 	}
         
         protected void doPost(HttpServletRequest request, 
